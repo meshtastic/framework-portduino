987656ca93b7202994b2dfe83517758ff4bf2139
Raibisch <61298065+raibisch@users.noreply.github.com>
2024-08-19 20:32:06 +0200

    Update PortduinoPrint.cpp
    fix bug with crash at Serial.printf
    
    look like missing "\0" at string end.

--- cores/portduino/PortduinoPrint.cpp
+++ cores/portduino/PortduinoPrint.cpp
@@ -3,9 +3,11 @@
 
 #include <cstdarg>
 
-#define MAX_STR_LEN 256
+//#define MAX_STR_LEN 256
+#define PRINTF_BUFFER_SIZE 250 // by JG
 
 size_t Print::printf(const char *format, ...) {
+  /* old:
   char buf[MAX_STR_LEN]; // FIXME, this burns a lot of stack, but on
                                 // Linux that is fine, TBD on MyNewt
   va_list args;
@@ -13,4 +15,19 @@ size_t Print::printf(const char *format, ...) {
   vsnprintf(buf, sizeof(buf), format, args);
   write(buf);
   va_end(args);
-}
+  */
+  
+  // by JG
+  char buf[PRINTF_BUFFER_SIZE];
+  va_list args;
+  va_start(args, format);
+  int status = vsnprintf(buf, PRINTF_BUFFER_SIZE, format, args);
+  va_end(args);
+  if (status >= 0) {
+    buf[PRINTF_BUFFER_SIZE - 1] = '\0';
+    size_t n = print(buf);
+    return n;
+  } else {
+    return 0;
+  }
+}     
